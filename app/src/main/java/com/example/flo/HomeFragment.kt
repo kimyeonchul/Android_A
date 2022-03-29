5701d02c557a7a45dbd63bb1f3d52f649b9015ae
kimyeonchul <yeonchul0807@nate.com>
2022-03-29 20:10:25 +0900

    2주차 과제까지 완성본

--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -21,6 +22,13 @@ class HomeFragment : Fragment() {
         binding.homeAlbumImgIv1.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
         }
+
+        val bannerAdaper = BannerVPAdapter(this)
+        bannerAdaper.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdaper.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter = bannerAdaper //뷰페이저와 어뎁터 연결
+        binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         return binding.root
     }
 }
